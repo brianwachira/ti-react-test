c733b798b2e6304e0e8a5752e634575f0bed89c4
brianwachira <bw@Eupotechs-MacBook-Air.local>
2021-09-08 13:38:35 +0300

    [FEATURE] : add crud services for user

---
+++ src/Services/users.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+// Crud functions for users
+
+    const url = "https://ti-react-test.herokuapp.com/users";
+// get users
+const getUsers = () => {
+    const request = axios.get(url)
+    return request.then(response =>  response.data);
+}
+
+// get single user
+const getUser = async id => {
+
+    const response = await axios.get(`${url}/${id}`)
+
+    return response.data
+}
+
+export default{ getUsers, getUser}
