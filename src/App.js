447859027ae81ba812996750ce6cdddefb3705a8
brianwachira <bw@Eupotechs-MacBook-Air.local>
2021-09-08 14:19:47 +0300

    [ENHANCEMENT] : initialize getting users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
-import React from "react"
+import React, { useEffect} from "react"
+import { useDispatch } from "react-redux";
 import { BrowserRouter, Switch, Route, Redirect, useRouteMatch} from "react-router-dom";
 
 //pages
 import PageNotFound from "./Pages/404/PageNotFound";
 import Home from "./Pages/Home/Home";
+import { initializeUsers } from "./State/usersReducer";
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(()=> {
+    //
+    dispatch(initializeUsers())
+  },[dispatch])
 
   //get id on route
   //const userMatch = useRouteMatch('/users/:id');
