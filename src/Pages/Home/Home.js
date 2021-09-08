513ead3d9075fb460d3d6038cae0827a7c997f0b
brianwachira <brianwachira7@gmail.com>
2021-09-08 21:33:36 +0300

    [ENHANCEMENT] : add skeleton

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,21 +1,37 @@
-import { useSelector } from "react-redux";
+/* eslint-disable react/react-in-jsx-scope */
+import { useSelector } from 'react-redux'
 
 const Home = () => {
 
     //get users from state
-    const users = useSelector(state => state);
+    const users = useSelector(state => state)
     return (
         <>
-            <p>Home</p>
-            <ul>
-                {users.map(user => 
-                    <li
-                        key={user.id}>
-                        {user.name}
-                    </li>)}
-            </ul>
+            <section>
+                <main className="container">
+                    <p>Home</p>
+                    <div className="row">
+                        {users.map(user =>
+                            <div
+                                className="col-md-3 mb-3"
+                                key={user.id}>
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h3 className="card-title">
+                                            {user.name}
+                                        </h3>
+                                        <p className={'card-text'}>BIO :{user.bio}</p>
+                                        <p className={'card-text'}>Email :{user.email}</p>
+                                        <p className={'card-text'}>Occupation :{user.occupation}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </main>
+            </section>
         </>
     )
 }
 
-export default Home;
+export default Home
