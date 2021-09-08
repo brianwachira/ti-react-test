0e5d1b361f179a912eaa00d9b0903b1246df747c
brianwachira <bw@Eupotechs-MacBook-Air.local>
2021-09-08 14:22:09 +0300

    [FEATURE] : initialize redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//redux provider
+import { Provider } from 'react-redux'
+import store from './State/store'
 import './Global.scss';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
   document.getElementById('root')
 );
 
