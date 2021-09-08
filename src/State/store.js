fc2bf9fd9a57d91eb158b892d9de0aed5a8fa4a7
brianwachira <bw@Eupotechs-MacBook-Air.local>
2021-09-08 14:23:38 +0300

    [ENHACNCEMENT] : add comments

--- src/State/store.js
+++ src/State/store.js
@@ -2,9 +2,7 @@ import { createStore, combineReducers, applyMiddleware}  from 'redux';
 import thunk from 'redux-thunk';
 import usersReducer from './usersReducer';
 
-const reducers = {
-    users : usersReducer
-}
+//create store for separation of concerns
 const store = createStore(usersReducer, applyMiddleware(thunk));
 
 export default store;
