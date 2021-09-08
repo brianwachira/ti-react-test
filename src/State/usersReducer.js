2e1be2e7927bd3a2117b49a36f34c3301b09dce1
brianwachira <brianwachira7@gmail.com>
2021-09-08 21:35:41 +0300

    [ENHANCEMENT] : add dispatch function for updating user data

--- src/State/usersReducer.js
+++ src/State/usersReducer.js
@@ -1,16 +1,19 @@
 import userService from '../Services/users'
-const usersReducer = ( state = [],action) => {
-    switch(action.type) {
+const usersReducer = (state = [], action) => {
+    switch (action.type) {
         case 'INIT_USERS':
             return action.data
         case 'SET_USER':
             return action.data
+        case 'UPDATE_USER':
+            return state.map(user => user.id === action.data.id ? action.data : user)
         default:
-            return state;
+            return state
     }
 }
 
-export const initializeUsers =  () => {
+//dispatch function to initialize users
+export const initializeUsers = () => {
     return async dispatch => {
         const users = await userService.getUsers()
 
@@ -21,6 +24,7 @@ export const initializeUsers =  () => {
     }
 }
 
+//dispatch function to get users
 export const getUser = id => {
     return async dispatch => {
         const user = await userService.getUser(id)
@@ -32,4 +36,17 @@ export const getUser = id => {
     }
 }
 
-export default usersReducer;
+//dispatch function to update user
+export const updateUser = (user, id) => {
+    return async dispatch => {
+
+        const updatedUser = await userService.updateUser(user, id)
+
+        dispatch({
+            type: 'UPDATE_USER',
+            data: updatedUser
+        })
+    }
+}
+
+export default usersReducer
