b242d34483f2117df48b09e9b7a69b4348ae953d
brianwachira <bw@Eupotechs-MacBook-Air.local>
2021-09-08 14:21:12 +0300

    [ENHANCEMENT] : get state data and display them on site

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,19 @@
+import { useSelector } from "react-redux";
+
 const Home = () => {
 
+    //get users from state
+    const users = useSelector(state => state);
     return (
         <>
             <p>Home</p>
+            <ul>
+                {users.map(user => 
+                    <li
+                        key={user.id}>
+                        {user.name}
+                    </li>)}
+            </ul>
         </>
     )
 }
