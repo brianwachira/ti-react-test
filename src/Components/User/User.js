8f9f3077a379636985fecee8a1b7163311a21a47
brianwachira <bw@Eupotechs-MacBook-Air.local>
2021-09-09 13:24:45 +0300

    [FEATURE] : add user content

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 //import ProfileCard from '../ProfileCard'
 import { useParams } from 'react-router-dom'
 import './User.scss'
-import { getUser, updateUser } from '../../State/usersReducer'
+import { getUser, setNotification, updateUser } from '../../State/usersReducer'
 import RegisterModal from '../ModalRegister/ModalRegister'
 import { useState } from 'react'
 
@@ -44,25 +44,47 @@ const User = () => {
                 occupation
             }
             dispatch(updateUser(updatedUser,user.id))
+            const notification = {
+                type : 'SUCCESS',
+                title: 'SUCCESS',
+                message: 'User updated successfully'
+            }
+            dispatch(setNotification(notification))
         }catch(error){
             alert(error)
         }
     }
     return (
         <>
-            <h1>{user && user.name}</h1>
-            <p>{user.bio}</p>
-            <RegisterModal
-            label={'edit details'}
-            name={name}
-            bio={bio}
-            email={email}
-            occupation={occupation}
-            setName={setName}
-            setBio={setBio}
-            setEmail={setEmail}
-            setOccupation={setOccupation}
-            handleSubmit={dispatchUpdate}/>
+            <div className="container d-flex justify-content-center">
+                <div className="profile-card card">
+                    <div className="d-flex flex-column flex-md-row">
+                        <div className="flex-shrink-0 mx-auto">
+                            <figure className={'d-flex align-items-center'}>
+                                <i className="fa fa-user text-center fa-3x text-light" aria-hidden="true"></i>
+                            </figure>
+                        </div>
+                        <div className="flex-grow-1 ms-md-3 mx-auto">
+                            <h3><i className="fa fa-user text-primary me-2 d-none d-md-inline-flex" aria-hidden="true"></i>{user.name}</h3>
+                            <p className={'me-4'}><i className="fa fa-id-card text-primary me-2 d-none d-md-inline-flex" aria-hidden="true"></i>{user.bio}</p>
+                            <p><i className="fa fa-envelope text-primary me-2 d-none d-md-inline-flex" aria-hidden="true"></i>{user.email}</p>
+                            <p><i className="fa fa-handshake-o text-primary me-2 d-none d-md-inline-flex" aria-hidden="true"></i>{user.occupation}</p>
+                            <RegisterModal
+                                label={'Edit User Details'}
+                                name={name}
+                                bio={bio}
+                                email={email}
+                                occupation={occupation}
+                                setName={setName}
+                                setBio={setBio}
+                                setEmail={setEmail}
+                                setOccupation={setOccupation}
+                                handleSubmit={dispatchUpdate}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
