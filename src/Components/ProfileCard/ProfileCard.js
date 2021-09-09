cc7614f72d2ad6df6473ff7d7604c1c51ea11199
brianwachira <bw@Eupotechs-MacBook-Air.local>
2021-09-09 16:14:55 +0300

    [FEATURE] : set profile card as separate component

---
+++ src/Components/ProfileCard/ProfileCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const ProfileCard =({ user }) => {
+
+    return (
+        <>
+        <div className="profile-card card">
+            <div className="d-flex flex-column flex-md-row">
+                <div className="flex-shrink-0 mx-auto">
+                    <figure className={'d-flex align-items-center'}>
+                        <i className="fa fa-user text-center fa-3x text-light" aria-hidden="true"></i>
+                    </figure>
+                </div>
+                <div className="flex-grow-1 ms-md-3 mx-auto">
+                    <h3><i className="fa fa-user text-primary me-2 d-none d-md-inline-flex" aria-hidden="true"></i>{user.name}</h3>
+                    <div className="d-flex justify-content-center justify-content-md-start">
+                        <Link to={`user/${user.id}`}>See More</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        </>
+    )
+}
+
+export default ProfileCard
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,12 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import ProfileCard from '../../Components/ProfileCard/ProfileCard'
 
 const Home = () => {
 
-
+    // fetch users from state
     const { users } = useSelector(state => state)
-    console.log(useSelector(state => state))
     return (
         <>
             <section>
@@ -18,21 +17,7 @@ const Home = () => {
                             <div
                                 className="col-md-6 col-lg-4 mb-2"
                                 key={user.id}>
-                                <div className="profile-card card">
-                                    <div className="d-flex flex-column flex-md-row">
-                                        <div className="flex-shrink-0 mx-auto">
-                                            <figure className={'d-flex align-items-center'}>
-                                                <i className="fa fa-user text-center fa-3x text-light" aria-hidden="true"></i>
-                                            </figure>
-                                        </div>
-                                        <div className="flex-grow-1 ms-md-3 mx-auto">
-                                            <h3><i className="fa fa-user text-primary me-2 d-none d-md-inline-flex" aria-hidden="true"></i>{user.name}</h3>
-                                            <div className="d-flex justify-content-center justify-content-md-start">
-                                                <Link to={`user/${user.id}`}>See More</Link>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                    <ProfileCard user={user}/>
                             </div>
                         )}
                     </div>
