cb0f5c3174b5ce144fc9664c8da30f3a61c1498b
brianwachira <bw@Eupotechs-MacBook-Air.local>
2021-09-09 10:09:49 +0300

    [ENHANCMENT] : refactor state and add updateUser function

--- src/State/usersReducer.js
+++ src/State/usersReducer.js
@@ -1,19 +1,38 @@
 import userService from '../Services/users'
-const usersReducer = (state = [], action) => {
-    switch (action.type) {
+const initialUser = {
+    id: null,
+    name: '',
+    bio: '',
+    occupation: ''
+}
+const initialState = {
+    user: initialUser,
+    users: []
+}
+export const usersReducer = ( state = initialState,action) => {
+    switch(action.type) {
         case 'INIT_USERS':
-            return action.data
+            return {
+                ...state,
+                users: action.data
+            }
         case 'SET_USER':
-            return action.data
+
+            return {
+                ...state,
+                user: action.data
+            }
         case 'UPDATE_USER':
-            return state.map(user => user.id === action.data.id ? action.data : user)
+            return {
+                user: action.data,
+                users: state.users.map(user => user.id === action.data.id ? action.data : user )
+            }
         default:
             return state
     }
 }
 
-//dispatch function to initialize users
-export const initializeUsers = () => {
+export const initializeUsers =  () => {
     return async dispatch => {
         const users = await userService.getUsers()
 
@@ -24,7 +43,6 @@ export const initializeUsers = () => {
     }
 }
 
-//dispatch function to get users
 export const getUser = id => {
     return async dispatch => {
         const user = await userService.getUser(id)
@@ -36,10 +54,8 @@ export const getUser = id => {
     }
 }
 
-//dispatch function to update user
-export const updateUser = (user, id) => {
+export const updateUser = (user,id) => {
     return async dispatch => {
-
         const updatedUser = await userService.updateUser(user, id)
 
         dispatch({
@@ -48,5 +64,4 @@ export const updateUser = (user, id) => {
         })
     }
 }
-
 export default usersReducer
